953f7656f98b94001c6da8477b665308f12e6414
Alexander Walther <asushofman@gmail.com>
2022-04-07 01:05:19 +0300

    Create reload_sound_device.sh

---
+++ reload_sound_device.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ ! $(whoami)="root" ]; then
+    exec sudo "$0" "$@"
+    echo "Error: failed to execute sudo" >&2
+    exit 1
+fi
+
+audio_device=$(lspci -vvv | grep "Audio" | awk -F' +' '{print $1}')
+echo "Sound device identifier: \"$audio_device\""
+
+hardware_device=$(ls /sys/bus/pci/devices/ | grep "$audio_device")
+echo "Hardware device identifier: \"$hardware_device\""
+
+echo 1 > /sys/bus/pci/devices/"$hardware_device"/remove
+echo "Marked audio device \"$hardware_device\" as removed"
+
+echo "Rescanning devices"
+echo 1 > /sys/bus/pci/rescan
+echo "Done."
